e8e6e3d91c2834fd00eb8e966f9f3ef8003b4514
zeeen <zeeen@DESKTOP-GP9JGLO>
2024-12-26 09:47:35 +0300

    Add project files.

---
+++ OOPthree/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPthree.Models
+{
+    public class Product
+    {
+        string name;
+        double price, quantity;
+        
+        public void SetName(string Name) { name = Name; }
+
+
+        public string GetName() { return name; }
+
+        public string GetInfo(string name, double price)
+        {
+            return "Name: " + name + " " + " Price :" + price;
+        }
+
+        public static string TotInfo(string name, double price, double quantity)
+        {
+
+            double Total = price * quantity;
+   
+            return $"product  {name } , price { price} , quantity {quantity }, Total { Total } ";
+        }
+
+    }
+}
---
+++ OOPthree/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+using OOPthree.Models;
+
+Product p = new Product();
+Console.WriteLine("enter name");
+p.SetName(Console.ReadLine());
+
+Console.WriteLine("Product name:  " + p.GetName());
+
+Console.WriteLine("enter price");
+double price = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Product name:  " + p.GetName() +" Product Price: " + price);
+double Total=0;
+
+Console.WriteLine("enter quantity");
+double quantity = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Product name:  " + p.GetName() + "Product Price: " + price +"quantity is: " + quantity + " total " + Total);
+
+Console.WriteLine(Product.TotInfo(p.GetName(),price, quantity));
